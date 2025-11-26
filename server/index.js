9b753c1d7bbb18188e4214db7ebf275c77e46421
Gabriele Lo Re <gabrolr70@gmail.com>
2025-11-26 19:15:13 +0100

    user router

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { routerUsers } from './routers/routerUsers/routerUsers.js';
+import { initializeDatabase } from './database/dbInit.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api/users', routerUsers);
+
+//decommentare questa linea per inizializzare il db!
+//await initializeDatabase();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
