2399d24b4e29915f2a8d5dea014a53637fda24c4
Gabriele Lo Re <gabrolr70@gmail.com>
2025-12-02 12:40:23 +0100

    p

--- wise-client/src/app/app.config.ts
+++ wise-client/src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { JWT_OPTIONS,JwtHelperService } from '@auth0/angular-jwt';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+function tokenGetter() {
+  return localStorage.getItem('accessToken');
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    // provideClientHydration(withEventReplay())
+    JwtHelperService,
+    { provide: JWT_OPTIONS, useValue: { tokenGetter } },
   ]
 };
--- wise-client/src/app/form-login/form-login.ts
+++ wise-client/src/app/form-login/form-login.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { IdService } from '../id.service';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../auth.service';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-login.html',
   styleUrl: './form-login.scss',
 })
 export class FormLogin {
 
+  constructor(private IdService: IdService,private readonly http: HttpClient,private readonly authService: AuthService,private readonly router:Router) {}
+  email = new FormControl('');
+  password = new FormControl('');
+
+  async login()
+  {
+    //console.log(this.email.value);
+     const formData = {
+      email: this.email.value,
+      password: this.password.value
+     };
+      this.http.post('http://localhost:1337/api/auth/login/', formData).subscribe({
+          next: (data: any) => {
+            alert('Login successful!');
+                this.authService.setAccessToken(data.token);
+                this.router.navigate(['/']);
+          },
+          error: (error) => {
+            alert('Login failed: ' + error.error.message);
+          }
+        });
+
+  }
+
+
+  
 }
